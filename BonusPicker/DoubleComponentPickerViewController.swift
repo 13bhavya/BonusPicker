c0785fdfb40563e92a30487d57796200a51454db
Bhavya Shah <bhavyashah@Bhavyas-iMac.local>
2019-10-14 01:48:12 -0700

    final

--- BonusPicker/DoubleComponentPickerViewController.swift
+++ BonusPicker/DoubleComponentPickerViewController.swift
@@ -8,7 +8,36 @@
 
 import UIKit
 
-class DoubleComponentPickerViewController: UIViewController {
+class DoubleComponentPickerViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    @IBOutlet weak var doublePicker: UIPickerView!
+    private let fillingComponent = 0
+    private let breadComponent = 1
+    private let fillingTypes = [
+    "Salad","Peanut Butter","Chesse Pizza","Macaroni","Falafals","Veggie Pattie","Vegemite"]
+    private let breadTypes = ["White","Whole Wheat","Rye","Sourdough","Seven Grain"]
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 2
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if component == breadComponent{
+            return breadTypes.count
+        } else {
+            return fillingTypes.count
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String?{
+        if component == breadComponent {
+            return breadTypes[row]
+        } else {
+            return fillingTypes[row]
+        }
+        
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,6 +46,22 @@ class DoubleComponentPickerViewController: UIViewController {
     }
     
 
+    @IBAction func onButtonPressed(_ sender: UIButton) {
+        let fillingRow = doublePicker.selectedRow(inComponent: fillingComponent)
+        let breadRow = doublePicker.selectedRow(inComponent: breadComponent)
+        
+        let filling = fillingTypes[fillingRow]
+        let bread = breadTypes[breadRow]
+        let message = "Your \(filling) on \(bread) will be right up"
+        
+        let alert = UIAlertController(
+        title: "Thank you for the order", message: message, preferredStyle: .alert)
+        let action = UIAlertAction(
+        title: "Great", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        
+    }
     /*
     // MARK: - Navigation
 
--- BonusPicker/SingleComponentPickerViewController.swift
+++ BonusPicker/SingleComponentPickerViewController.swift
@@ -11,16 +11,23 @@ import UIKit
 class SingleComponentPickerViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     
     @IBOutlet weak var singlePicker: UIPickerView!
+    private let characterNames = ["Luke","Leia","Han","Chewbacca","Artoo","Threepio","Lando"]
     
+    //Mark: Picker Data Source Methods
+
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        <#code#>
+        return 1
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        <#code#>
+        return characterNames.count
     }
+    //Marks: Picker Delegate Methods
     
-
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String?{
+        return characterNames[row]
+    
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -37,5 +44,20 @@ class SingleComponentPickerViewController: UIViewController, UIPickerViewDelegat
         // Pass the selected object to the new view controller.
     }
     */
-
+    @IBAction func onButtonPressed(_ sender: UIButton) {
+        let row = singlePicker.selectedRow(inComponent: 0)
+        let selected = characterNames[row]
+        let title = "You selected \(selected)!"
+        let alert = UIAlertController(
+        title: title, message: "Thank you for choosing", preferredStyle: .alert)
+        let action = UIAlertAction(
+        title: "You are welcome", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
 }
+
+
+
+
