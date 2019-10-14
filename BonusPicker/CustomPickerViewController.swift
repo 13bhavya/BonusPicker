62f5baf579e925763be7adbd160097c5ee1fb409
Bhavya Shah <bhavyashah@Bhavyas-iMac.local>
2019-10-13 23:37:11 -0700

    init

---
+++ BonusPicker/CustomPickerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CustomPickerViewController.swift
+//  BonusPicker
+//
+//  Created by Bhavya Shah on 13/10/19.
+//  Copyright © 2019 Bhavya Shah. All rights reserved.
+//
+
+import UIKit
+
+class CustomPickerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
